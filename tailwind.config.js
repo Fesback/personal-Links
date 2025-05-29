0dd80e7771768b6ae8e4f768c9c32f10385bd4a6
Sebastian Bismarck <134103429+Fesback@users.noreply.github.com>
2025-05-28 22:25:44 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./**/*.html",
+    // agrega aquí cualquier otra carpeta o archivo HTML que uses
+  ],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+}
